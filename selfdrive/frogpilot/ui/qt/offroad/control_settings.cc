BRid37/opAIO

More aggressive follow

selfdrive/frogpilot/ui/qt/offroad/control_settings.cc
@@ -311,9 +311,9 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
                relaxedPersonalityKeys.find(param) != relaxedPersonalityKeys.end()) {
       if (param == "TrafficFollow" || param == "AggressiveFollow" || param == "StandardFollow" || param == "RelaxedFollow") {
         if (param == "TrafficFollow") {
-          toggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.5, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
+          toggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.10, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
         } else {
-          toggle = new FrogPilotParamValueControl(param, title, desc, icon, 1, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
+          toggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.50, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
         }
       } else {
         toggle = new FrogPilotParamValueControl(param, title, desc, icon, 1, 500, std::map<int, QString>(), this, false, "%");

BRid37/openpilot

selfdrive/frogpilot/ui/qt/offroad/control_settings.cc
@@ -302,9 +302,9 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
                relaxedPersonalityKeys.find(param) != relaxedPersonalityKeys.end()) {
       if (param == "TrafficFollow" || param == "AggressiveFollow" || param == "StandardFollow" || param == "RelaxedFollow") {
         if (param == "TrafficFollow") {
-          controlToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.5, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
+          controlToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.10, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
         } else {
-          controlToggle = new FrogPilotParamValueControl(param, title, desc, icon, 1, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
+          controlToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.50, 5, std::map<int, QString>(), this, false, tr(" seconds"), 1, 0.01);
         }
       } else {
         controlToggle = new FrogPilotParamValueControl(param, title, desc, icon, 1, 500, std::map<int, QString>(), this, false, "%");